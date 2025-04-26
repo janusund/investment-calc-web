0c1925ea94b523519c877468afa640ca26e64484
js <janu.sun@gmail.com>
2025-04-26 07:56:24 -0700

    Added screenshots

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header id ="header">
       <img src={headerImg} />
-      <h1>React Investment Calculator</h1>
+      <h1>Investment Calculator</h1>
     </header>
   );
 }
